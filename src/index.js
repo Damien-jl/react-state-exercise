4dd7f52aba354fc188cdaddb4c27c0f61bfd7bef
Damien Legg <leggdamien@gmail.com>
2023-09-05 10:52:43 -0500

    had to add some comments and clean up some

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-*/
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
